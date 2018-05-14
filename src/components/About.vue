7237c5bd5545564de7db945aeeb311026f1ab008
Herve <h.hochart29@gmail.com>
2018-05-14 23:59:20 +0200

    :snake: linted

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,54 +2,53 @@
   <div class="relative">
     <div class="container">
 
-        <h3>Qui-suis-je ?</h3>
+      <h3>Qui-suis-je ?</h3>
 
-    <div class="container whoami-container">
-      <div id="about-img" v-if="this.page.whoami_img">
-        <img :src="this.page.whoami_img.url" alt="">
-      </div>
-
-      <div id="about-desc" v-html="this.page.whoami_content"></div>
-
-      <div class="activities" v-if="this.page.whoami_passion">
-        <div class="activity" v-for="passion in this.page.whoami_passion">
-          <img :src="passion.icon.url" :alt="passion.text">
-          <div class="activity-name">{{ passion.text }}</div>
+      <div class="container whoami-container">
+        <div id="about-img" v-if="this.page.whoami_img">
+          <img :src="this.page.whoami_img.url" alt="">
         </div>
 
-      </div>
-    </div>
+        <div id="about-desc" v-html="this.page.whoami_content"></div>
 
-    <h3>Parcours</h3>
-    <div class="ecoles">
-      <div class="ecole" v-for="ecole in this.page.ecole">
-        <div class="left-wrapper">
-          <div class="ecole-name">
-            {{ ecole.title }}
+        <div class="activities" v-if="this.page.whoami_passion">
+          <div class="activity" v-for="passion in this.page.whoami_passion">
+            <img :src="passion.icon.url" :alt="passion.text">
+            <div class="activity-name">{{ passion.text }}</div>
           </div>
-          <div class="ecole-date">
-            {{ ecole.date }}
-          </div>
-          <div class="ecole-skill" v-for="competence in ecole.competences">
-            {{ competence.skill }}
-          </div>
+
         </div>
-        <div class="right-wrapper">
-          <div class="ecole-img" :style="{ background: `url(${ecole.img.url})` }">
-            <div class="ecole-lieu">
-              {{ ecole.lieu }}
+      </div>
+
+      <h3>Parcours</h3>
+      <div class="ecoles">
+        <div class="ecole" v-for="ecole in this.page.ecole">
+          <div class="left-wrapper">
+            <div class="ecole-name">
+              {{ ecole.title }}
+            </div>
+            <div class="ecole-date">
+              {{ ecole.date }}
+            </div>
+            <div class="ecole-skill" v-for="competence in ecole.competences">
+              {{ competence.skill }}
+            </div>
+          </div>
+          <div class="right-wrapper">
+            <div class="ecole-img" :style="{ background: `url(${ecole.img.url})` }">
+              <div class="ecole-lieu">
+                {{ ecole.lieu }}
+              </div>
             </div>
           </div>
         </div>
-        </div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Axios from 'axios'
 import BackToTop from '@/components/sub/BackToTop'
-
 
 export default {
   components: {BackToTop},
